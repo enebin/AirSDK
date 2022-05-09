895cec42977fb93bb3a4a74cefd2b939201f9b55
enebin <lyb35548@gmail.com>
2022-05-09 16:03:27 +0900

    Initial Commit

---
+++ AirSDK/AirSDK.h
@@ -0,0 +1,18 @@
+//
+//  AirSDK.h
+//  AirSDK
+//
+//  Created by 이영빈 on 2022/05/09.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AirSDK.
+FOUNDATION_EXPORT double AirSDKVersionNumber;
+
+//! Project version string for AirSDK.
+FOUNDATION_EXPORT const unsigned char AirSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AirSDK/PublicHeader.h>
+
+
